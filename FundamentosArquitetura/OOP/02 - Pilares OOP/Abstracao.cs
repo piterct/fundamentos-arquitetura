4ce07b8742f4565a42618caa7c4d2b3afe194a9d
Michael Peter <michael_piterct@hotmail.com>
2022-02-11 00:23:18 -0300

    Abstracao

---
+++ FundamentosArquitetura/OOP/02 - Pilares OOP/Abstracao.cs
@@ -0,0 +1,22 @@
+﻿namespace OOP._02___Pilares_OOP
+{
+    public abstract class Eletrodomestico
+    {
+        private readonly string _nome;
+        private readonly int _voltagem;
+
+        protected Eletrodomestico(string nome, int voltagem)
+        {
+            _nome = nome;
+            _voltagem = voltagem;
+        }
+
+        public abstract void Ligar();
+        public abstract void Desligar();
+
+        public virtual void Testar()
+        {
+            // teste do equipamento
+        }
+    }
+}
