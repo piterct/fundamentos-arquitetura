367b8840c9c32a41ca6df4f16ad48597e30fe519
Michael Peter <michael_piterct@hotmail.com>
2022-02-13 00:09:17 -0300

    Pilares OOP

---
+++ FundamentosArquitetura/OrientacaoObjetos/02 - Pilares OOP/04 -Polimorfismo.cs
@@ -0,0 +1,36 @@
+﻿namespace OrientacaoObjetos._02___Pilares_OOP
+{
+    public class CafeteiraExpressa : Eletrodomestico
+    {
+        public CafeteiraExpressa(string nome, int voltagem) : base(nome, voltagem) { }
+
+        public CafeteiraExpressa()
+            : base("Padrao", 220) { }
+
+        private static void AquecerAgua() { }
+
+        private static void MoerGraos() { }
+
+        public void PrepararCafe()
+        {
+            Testar();
+            AquecerAgua();
+            MoerGraos();
+            // ETC...
+        }
+        public override void Testar()
+        {
+            // teste de cafeteira
+        }
+
+        public override void Ligar()
+        {
+            // Verificar recipiente de agua
+        }
+
+        public override void Desligar()
+        {
+            // Resfriar aquecedor
+        }
+    }
+}
---
+++ FundamentosArquitetura/OrientacaoObjetos/02 - Pilares OOP/05- Encapsulamento.cs
@@ -0,0 +1,13 @@
+﻿namespace OrientacaoObjetos._02___Pilares_OOP
+{
+    public class AutomacaoCafe
+    {
+        public void ServirCafe()
+        {
+            var espresso = new CafeteiraExpressa();
+            espresso.Ligar();
+            espresso.PrepararCafe();
+            espresso.Desligar();
+        }
+    }
+}
