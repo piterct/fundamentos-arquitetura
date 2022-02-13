7cb50c30779921cbb4965fada713577caf602ea9
Michael Peter <michael_piterct@hotmail.com>
2022-02-12 23:58:22 -0300

    Encapsulamento

---
+++ FundamentosArquitetura/OOP/02 - Pilares OOP/05- Encapsulamento.cs
@@ -0,0 +1,13 @@
+﻿namespace OOP._02___Pilares_OOP
+{
+    public  class AutomacaoCafe
+    {
+        public void ServirCafe()
+        {
+            var espresso = new CafeteiraExpressa();
+            espresso.Ligar();
+            espresso.PrepararCafe();
+            espresso.Desligar();
+        }
+    }
+}
