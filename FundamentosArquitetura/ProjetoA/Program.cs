639a3319f61442bf1561968a31c6df58dbdc35a3
Michael Peter <michael_piterct@hotmail.com>
2022-02-13 00:13:46 -0300

    projectA modificadores

---
+++ FundamentosArquitetura/ProjetoA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
