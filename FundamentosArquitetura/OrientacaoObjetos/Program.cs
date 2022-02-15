4f1124a91a3c8b92c5bae2b0175d4b7b52dc9d94
Michael Peter <michael_piterct@hotmail.com>
2022-02-14 21:06:11 -0300

    operation test

--- FundamentosArquitetura/OrientacaoObjetos/Program.cs
+++ FundamentosArquitetura/OrientacaoObjetos/Program.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using OrientacaoObjetos._02___Pilares_OOP;
+using OrientacaoObjetos._04___HerancaVsComposicao;
+using OrientacaoObjetos.InterfaceVsImplementacao;
+using System;
 
 namespace OrientacaoObjetos
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Escolha a operação:");
+            Console.WriteLine("");
+            Console.WriteLine("1 - Encapsulamento");
+            Console.WriteLine("2 - Heranca Composicao 1");
+            Console.WriteLine("3 - Heranca Composicao 2");
+            Console.WriteLine("4 - Interface Implementacao");
+
+            var opcao = Console.ReadKey();
+
+            switch (opcao.KeyChar)
+            {
+                case '1':
+                    new AutomacaoCafe().ServirCafe();
+                    break;
+                case '2':
+                    new TestesHerancaComposicao();
+                    break;
+                case '3':
+                    new TestesHerancaComposicao2();
+                    break;
+                case '4':
+                    new TesteInterfaceImplementacao();
+                    break;
+            }
+
+            Main();
         }
     }
 }
