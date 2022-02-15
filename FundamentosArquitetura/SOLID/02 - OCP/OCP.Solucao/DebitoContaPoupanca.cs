8a441205b271f025842c3eabab5fc28b63028e59
Michael Peter <michael_piterct@hotmail.com>
2022-02-14 22:22:55 -0300

    DebitoContaPoupanca

---
+++ FundamentosArquitetura/SOLID/02 - OCP/OCP.Solucao/DebitoContaPoupanca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SOLID._02___OCP.OCP.Solucao
+{
+    public class DebitoContaPoupanca : DebitoConta
+    {
+        public override string Debitar(decimal valor, string conta)
+        {
+            // Valida Aniversário da Conta
+            // Debita Conta Corrente
+            return FormatarTransacao();
+        }
+    }
+}
