61b886a5175cece43bdfbc213334381103ce9715
Michael Peter <michael_piterct@hotmail.com>
2022-02-15 00:05:58 -0300

    LSP Retangulo

---
+++ FundamentosArquitetura/SOLID/03 - LSP/LSP.Solucao/Retangulo.cs
@@ -0,0 +1,11 @@
+﻿namespace SOLID._03___LSP.LSP.Solucao
+{
+    public class Retangulo : Paralelogramo
+    {
+        public Retangulo(int altura, int largura)
+            : base(altura, largura)
+        {
+
+        }
+    }
+}
