04b366ef3caa0ba77288e8dc027ef517bbe9fafc
Michael Peter <michael_piterct@hotmail.com>
2022-02-14 22:03:05 -0300

    Cliente service

---
+++ FundamentosArquitetura/SOLID/01 - SRP/SRP.Solucao/ClienteService.cs
@@ -0,0 +1,18 @@
+﻿namespace SOLID._01___SRP.SRP.Solucao
+{
+    public class ClienteService
+    {
+        public string AdicionarCliente(Cliente cliente)
+        {
+            if (!cliente.Validar())
+                return "Dados inválidos";
+
+            var repo = new ClienteRepository();
+            repo.AdicionarCliente(cliente);
+
+            EmailService.Enviar("empresa@empresa.com", cliente.Email.Endereco, "Bem Vindo", "Parabéns está Cadastrado");
+
+            return "Cliente cadastrado com sucesso";
+        }
+    }
+}
