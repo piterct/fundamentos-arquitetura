62a4e66c12d24e221d4aded22d2363f50ac37da2
Michael Peter <michael_piterct@hotmail.com>
2022-02-14 22:21:52 -0300

    DebitoContaInvestimento

--- FundamentosArquitetura/SOLID/02 - OCP/OCP.Solucao/DebitoConta.cs
+++ FundamentosArquitetura/SOLID/02 - OCP/OCP.Solucao/DebitoConta.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace SOLID._02___OCP.OCP.Solucao
 {
---
+++ FundamentosArquitetura/SOLID/02 - OCP/OCP.Solucao/DebitoContaInvestimento.cs
@@ -0,0 +1,12 @@
+﻿namespace SOLID._02___OCP.OCP.Solucao
+{
+    public class DebitoContaInvestimento : DebitoConta
+    {
+        public override string Debitar(decimal valor, string conta)
+        {
+            // Debita Conta Investimento
+            // Isentar Taxas
+            return FormatarTransacao();
+        }
+    }
+}
