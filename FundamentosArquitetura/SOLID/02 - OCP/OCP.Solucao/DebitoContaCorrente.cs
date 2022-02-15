0fdbc003ac3ab02cb5e717d6370b60b89c9866da
Michael Peter <michael_piterct@hotmail.com>
2022-02-14 22:13:33 -0300

    DebitoContaCorrente

---
+++ FundamentosArquitetura/SOLID/02 - OCP/OCP.Solucao/DebitoContaCorrente.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOLID._02___OCP.OCP.Solucao
+{
+    public class DebitoContaCorrente : DebitoConta
+    {
+        public override string Debitar(decimal valor, string conta)
+        {
+            // Debita Conta Corrente
+            return FormatarTransacao();
+        }
+    }
+}
