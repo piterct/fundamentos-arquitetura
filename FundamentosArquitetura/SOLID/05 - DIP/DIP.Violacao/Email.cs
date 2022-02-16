14ede8e0b98e5b3ebf9981c183183c3aeeb1eb1b
Michael Peter <michael_piterct@hotmail.com>
2022-02-15 23:35:57 -0300

    drip solid

---
+++ FundamentosArquitetura/SOLID/05 - DIP/DIP.Violacao/Email.cs
@@ -0,0 +1,12 @@
+﻿namespace SOLID._05___DIP.DIP.Violacao
+{
+    public class Email
+    {
+        public string Endereco { get; set; }
+
+        public bool Validar()
+        {
+            return Endereco.Contains("@");
+        }
+    }
+}
