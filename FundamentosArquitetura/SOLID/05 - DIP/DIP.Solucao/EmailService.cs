fe13ec09b2593fbdefa03792cca6a516b2b1c402
Michael Peter <michael_piterct@hotmail.com>
2022-02-15 23:43:45 -0300

    Dip IEmailServices solution

---
+++ FundamentosArquitetura/SOLID/05 - DIP/DIP.Solucao/EmailService.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Mail;
+
+namespace SOLID._05___DIP.DIP.Solucao
+{
+    public class EmailServices : IEmailServices
+    {
+        public void Enviar(string de, string para, string assunto, string mensagem)
+        {
+            var mail = new MailMessage(de, para);
+            var client = new SmtpClient
+            {
+                Port = 25,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Host = "smtp.google.com"
+            };
+
+            mail.Subject = assunto;
+            mail.Body = mensagem;
+            client.Send(mail);
+        }
+    }
+}
