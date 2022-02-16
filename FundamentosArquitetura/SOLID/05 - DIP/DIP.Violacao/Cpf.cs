2b41e269cc0c1819e00746588351c3a19cb74765
Michael Peter <michael_piterct@hotmail.com>
2022-02-15 23:35:39 -0300

    dip cliente and cpf  violation

---
+++ FundamentosArquitetura/SOLID/05 - DIP/DIP.Violacao/Cpf.cs
@@ -0,0 +1,12 @@
+﻿namespace SOLID._05___DIP.DIP.Violacao
+{
+    public class Cpf
+    {
+        public string Numero { get; set; }
+
+        public bool Validar()
+        {
+            return Numero.Length == 11;
+        }
+    }
+}
