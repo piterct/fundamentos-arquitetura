2c8626a9026ec10bc12840a8da475033288acf8f
Michael Peter <michael_piterct@hotmail.com>
2022-02-16 00:03:21 -0300

    DIP cliente services solution

---
+++ FundamentosArquitetura/SOLID/05 - DIP/DIP.Solucao/ClienteServices.cs
@@ -0,0 +1,38 @@
+﻿using SOLID._05___DIP.DIP.Solucao.Interfaces;
+
+namespace SOLID._05___DIP.DIP.Solucao
+{
+    public class ClienteServices : IClienteServices
+    {
+        private readonly IClienteRepository _clienteRepository;
+        private readonly IEmailServices _emailServices;
+
+        public ClienteServices(
+            IEmailServices emailServices,
+            IClienteRepository clienteRepository)
+        {
+            _emailServices = emailServices;
+            _clienteRepository = clienteRepository;
+        }
+
+        public string AdicionarCliente(Cliente cliente)
+        {
+            if (!cliente.Validar())
+                return "Dados inválidos";
+
+            _clienteRepository.AdicionarCliente(cliente);
+
+            _emailServices.Enviar("empresa@empresa.com", cliente.Email.Endereco, "Bem Vindo", "Parabéns está Cadastrado");
+
+            return "Cliente cadastrado com sucesso";
+        }
+    }
+
+    public class TesteDip
+    {
+        public TesteDip()
+        {
+            var cliService = new ClienteServices(new EmailServices(), new ClienteRepository2());
+        }
+    }
+}
