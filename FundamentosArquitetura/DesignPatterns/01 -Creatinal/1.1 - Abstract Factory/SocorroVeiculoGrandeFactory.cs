5bb5848e6898ff587c2b5d86332ff8918d7a5247
Michael Peter <michael_piterct@hotmail.com>
2022-03-01 00:50:30 -0300

    Socorro Veiculo Grande Factory

---
+++ FundamentosArquitetura/DesignPatterns/01 -Creatinal/1.1 - Abstract Factory/SocorroVeiculoGrandeFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns._01__Creatinal._1._1___Abstract_Factory
+{
+    // Concrete Factory
+    public class SocorroVeiculoGrandeFactory : AutoSocorroFactory
+    {
+        public override Guincho CriarGuincho()
+        {
+            return GuinchoCreator.Criar(Porte.Grande);
+        }
+
+        public override Veiculo CriarVeiculo(string modelo, Porte porte)
+        {
+            return VeiculoCreator.Criar(modelo, porte);
+        }
+    }
+}
