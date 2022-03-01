913f7898c57aea94d54c3a05cfd061f09a9443ab
Michael Peter <michael_piterct@hotmail.com>
2022-03-01 00:43:59 -0300

    AutoSocorroFactory

---
+++ FundamentosArquitetura/DesignPatterns/01 -Creatinal/1.1 - Abstract Factory/AutoSocorroFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPatterns._01__Creatinal._1._1___Abstract_Factory
+{
+    // Abstract Factory
+    public abstract class AutoSocorroFactory
+    {
+        public abstract Guincho CriarGuincho();
+        public abstract Veiculo CriarVeiculo(string modelo, Porte porte);
+    }
+}
