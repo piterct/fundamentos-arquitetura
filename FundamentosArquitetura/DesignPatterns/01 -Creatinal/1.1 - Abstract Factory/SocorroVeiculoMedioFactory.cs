da3c2d1fd3b9234d278c531948a5359d41e031fe
Michael Peter <michael_piterct@hotmail.com>
2022-03-01 00:49:22 -0300

    SocorroVeiculoMedioFactory

---
+++ FundamentosArquitetura/DesignPatterns/01 -Creatinal/1.1 - Abstract Factory/SocorroVeiculoMedioFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns._01__Creatinal._1._1___Abstract_Factory
+{
+    // Concrete Factory
+    public class SocorroVeiculoMedioFactory : AutoSocorroFactory
+    {
+        public override Guincho CriarGuincho()
+        {
+            return GuinchoCreator.Criar(Porte.Medio);
+        }
+
+        public override Veiculo CriarVeiculo(string modelo, Porte porte)
+        {
+            return VeiculoCreator.Criar(modelo, porte);
+        }
+    }
+}
