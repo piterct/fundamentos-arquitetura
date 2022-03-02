82aeeaae6287e2b82eca1b85208143888c3481ae
Michael Peter <michael_piterct@hotmail.com>
2022-03-01 21:59:48 -0300

    DbFactory

---
+++ FundamentosArquitetura/DesignPatterns/01 -Creatinal/1.2 - Factory Method/DbFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DesignPatterns._01__Creatinal._1._2___Factory_Method
+{
+    // Abstract Creator
+    public abstract class DbFactory
+    {
+        // Factory Method
+        public abstract DbConnector CreateConnector(string connectionString);
+
+        public static DbFactory Database(DataBase dataBase)
+        {
+            if (dataBase == DataBase.SqlServer)
+                return new SqlFactory();
+            if (dataBase == DataBase.Oracle)
+                return new OracleFactory();
+
+            throw new ApplicationException("Banco de dados não reconhecido.");
+        }
+    }
+}
