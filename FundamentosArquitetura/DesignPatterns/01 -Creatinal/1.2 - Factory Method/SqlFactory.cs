7ce4bbbca9dd74cae0c978345421572d9ff4b54a
Michael Peter <michael_piterct@hotmail.com>
2022-03-01 22:09:28 -0300

    SqlFactory and SqlServerConnector

---
+++ FundamentosArquitetura/DesignPatterns/01 -Creatinal/1.2 - Factory Method/SqlFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPatterns._01__Creatinal._1._2___Factory_Method
+{
+    // Concrete Creator
+    public class SqlFactory : DbFactory
+    {
+        // Factory Method
+        public override DbConnector CreateConnector(string connectionString)
+        {
+            return new SqlServerConnector(connectionString);
+        }
+    }
+}
---
+++ FundamentosArquitetura/DesignPatterns/01 -Creatinal/1.2 - Factory Method/SqlServerConnector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DesignPatterns._01__Creatinal._1._2___Factory_Method
+{
+    // Concrete Product
+    public class SqlServerConnector : DbConnector
+    {
+        public SqlServerConnector(string connectionString) : base(connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public override Connection Connect()
+        {
+            Console.WriteLine("Conectando ao banco SQL Server...");
+            var connection = new Connection(ConnectionString);
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
