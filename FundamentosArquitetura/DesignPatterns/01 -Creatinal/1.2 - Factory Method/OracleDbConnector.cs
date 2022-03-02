57a555468144041eeafbf2213742fd17ad6014ea
Michael Peter <michael_piterct@hotmail.com>
2022-03-01 22:18:15 -0300

    OracleDbConnector

---
+++ FundamentosArquitetura/DesignPatterns/01 -Creatinal/1.2 - Factory Method/OracleDbConnector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DesignPatterns._01__Creatinal._1._2___Factory_Method
+{
+    // Concrete Product
+    public class OracleDbConnector : DbConnector
+    {
+        public OracleDbConnector(string connectionString) : base(connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        public override Connection Connect()
+        {
+            Console.WriteLine("Conectando ao banco Oracle...");
+            var connection = new Connection(ConnectionString);
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
