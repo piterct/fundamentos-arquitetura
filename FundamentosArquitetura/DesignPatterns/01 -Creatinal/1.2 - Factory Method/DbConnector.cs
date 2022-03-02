4ad62e14cf479aa3781ef4386a92eb0fe2aeb70a
Michael Peter <michael_piterct@hotmail.com>
2022-03-01 21:59:07 -0300

    DbConnector

---
+++ FundamentosArquitetura/DesignPatterns/01 -Creatinal/1.2 - Factory Method/DbConnector.cs
@@ -0,0 +1,14 @@
+﻿namespace DesignPatterns._01__Creatinal._1._2___Factory_Method
+{
+    // Abstract Product
+    public abstract class DbConnector
+    {
+        protected DbConnector(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+
+        protected string ConnectionString { get; set; }
+        public abstract Connection Connect();
+    }
+}
