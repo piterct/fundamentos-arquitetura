24c31022c62d04679191ea45fca589ca7118f826
Michael Peter <michael_piterct@hotmail.com>
2022-03-02 23:18:43 -0300

    OracleFacrtory

---
+++ FundamentosArquitetura/DesignPatterns/01 -Creatinal/1.2 - Factory Method/OracleFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace DesignPatterns._01__Creatinal._1._2___Factory_Method
+{
+    // Concrete Creator
+    public class OracleFactory : DbFactory
+    {
+        // Factory Method
+        public override DbConnector CreateConnector(string connectionString)
+        {
+            return new OracleDbConnector(connectionString);
+        }
+    }
+}
