a8666f3880f73d8fe66d38964e6c84b86e5b93bc
Michael Peter <michael_piterct@hotmail.com>
2022-03-02 23:24:56 -0300

    Server

---
+++ FundamentosArquitetura/DesignPatterns/01 -Creatinal/1.3 Singleton/Server.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DesignPatterns._01__Creatinal._1._3_Singleton
+{
+    internal class Server
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string IP { get; set; }
+    }
+}
