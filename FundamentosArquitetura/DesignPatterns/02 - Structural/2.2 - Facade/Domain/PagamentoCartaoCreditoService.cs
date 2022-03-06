8d06309b87bfcde17ac13174a41f7e054f1e6f51
Michael Peter <michael_piterct@hotmail.com>
2022-03-05 23:10:23 -0300

    PagamentoCartaoCreditoService

---
+++ FundamentosArquitetura/DesignPatterns/02 - Structural/2.2 - Facade/Domain/PagamentoCartaoCreditoService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DesignPatterns._02___Structural._2._2___Facade.Domain
+{
+    public class PagamentoCartaoCreditoService : IPagamento
+    {
+        private readonly IPagamentoCartaoCreditoFacade _pagamentoCartaoCreditoFacade;
+
+        public PagamentoCartaoCreditoService(IPagamentoCartaoCreditoFacade pagamentoCartaoCreditoFacade)
+        {
+            _pagamentoCartaoCreditoFacade = pagamentoCartaoCreditoFacade;
+        }
+
+        public Pagamento RealizarPagamento(Pedido pedido, Pagamento pagamento)
+        {
+            pagamento.Valor = pedido.Produtos.Sum(p => p.Valor);
+            Console.WriteLine("Iniciando Pagamento via Cartão de Crédito - Valor R$ " + pagamento.Valor);
+
+            if (_pagamentoCartaoCreditoFacade.RealizarPagamento(pedido, pagamento))
+            {
+                pagamento.Status = "Pago via Cartão de Crédito";
+                return pagamento;
+            }
+
+            pagamento.Status = "Cartão de Crédito Recusado!";
+            return pagamento;
+        }
+    }
+}
