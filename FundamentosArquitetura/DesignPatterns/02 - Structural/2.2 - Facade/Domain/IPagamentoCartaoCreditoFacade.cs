db6d185f1b70228962fb9c8696492071d00ed751
Michael Peter <michael_piterct@hotmail.com>
2022-03-05 23:08:01 -0300

    IPagamentoCartaoCreditoFacade

---
+++ FundamentosArquitetura/DesignPatterns/02 - Structural/2.2 - Facade/Domain/IPagamentoCartaoCreditoFacade.cs
@@ -0,0 +1,7 @@
+﻿namespace DesignPatterns._02___Structural._2._2___Facade.Domain
+{
+    public interface IPagamentoCartaoCreditoFacade
+    {
+        bool RealizarPagamento(Pedido pedido, Pagamento pagamento);
+    }
+}
