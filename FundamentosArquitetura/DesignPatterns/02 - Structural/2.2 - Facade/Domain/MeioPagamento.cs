3049abb88b5593d4fd25c8a7f4f7cd157c2697b2
Michael Peter <michael_piterct@hotmail.com>
2022-03-05 22:57:23 -0300

    Meio Pagamento e Pagamento

---
+++ FundamentosArquitetura/DesignPatterns/02 - Structural/2.2 - Facade/Domain/MeioPagamento.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPatterns._02___Structural._2._2___Facade.Domain
+{
+    public enum MeioPagamento
+    {
+        CartaoCredito,
+        Boleto,
+        TransferenciaBancaria
+    }
+}
