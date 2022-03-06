4e335649e08a6d2f2eacaa6f4e25ffaea2183aa7
Michael Peter <michael_piterct@hotmail.com>
2022-03-05 22:53:36 -0300

    CrosCutting facede

---
+++ FundamentosArquitetura/DesignPatterns/02 - Structural/2.2 - Facade/CrossCutting/ConfigurationManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace DesignPatterns._02___Structural._2._2___Facade.CrossCutting
+{
+    public class ConfigurationManager : IConfigurationManager
+    {
+        public string GetValue(string node)
+        {
+            return new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 10)
+                .Select(s => s[new Random().Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ FundamentosArquitetura/DesignPatterns/02 - Structural/2.2 - Facade/CrossCutting/IConfigurationManager.cs
@@ -0,0 +1,7 @@
+﻿namespace DesignPatterns._02___Structural._2._2___Facade.CrossCutting
+{
+    public interface IConfigurationManager
+    {
+        string GetValue(string node);
+    }
+}
