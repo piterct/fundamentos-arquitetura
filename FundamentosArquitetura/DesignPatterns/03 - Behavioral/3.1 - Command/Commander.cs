681259ea54390decca58c564ea5856727b13eb19
Michael Peter <michael_piterct@hotmail.com>
2022-03-07 23:52:05 -0300

    Commander

---
+++ FundamentosArquitetura/DesignPatterns/03 - Behavioral/3.1 - Command/Commander.cs
@@ -0,0 +1,8 @@
+﻿namespace DesignPatterns._03___Behavioral._3._1___Command
+{
+    internal abstract class Commander
+    {
+        public abstract void Executar();
+        public abstract void Desfazer();
+    }
+}
