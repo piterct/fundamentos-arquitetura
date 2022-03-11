cf3afffbb0bd72b97f498da4f440e3b15cad6185
Michael Peter <michael_piterct@hotmail.com>
2022-03-10 23:34:46 -0300

    3.3 - Observable

---
+++ FundamentosArquitetura/DesignPatterns/03 - Behavioral/3.3 - Observable/IObservador.cs
@@ -0,0 +1,9 @@
+﻿namespace DesignPatterns._03___Behavioral._3._3___Observable
+{
+    // Observer
+    public interface IObservador
+    {
+        string Nome { get; }
+        void Notificar(Investimento investimento);
+    }
+}
---
+++ FundamentosArquitetura/DesignPatterns/03 - Behavioral/3.3 - Observable/PapelBovespa.cs
@@ -0,0 +1,11 @@
+﻿namespace DesignPatterns._03___Behavioral._3._3___Observable
+{
+    // Concrete Subject
+    public class PapelBovespa : Investimento
+    {
+        public PapelBovespa(string simbolo, decimal preco)
+            : base(simbolo, preco)
+        {
+        }
+    }
+}
